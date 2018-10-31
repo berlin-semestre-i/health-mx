fb82a2fdc02838c76d24f77b5ce3e5bfa6225819
julia <juliapaola97@gmail.com>
2018-10-31 09:11:09 -0600

    feat(boilerplate): adds medic and beneficiary boilerplate

---
+++ components/Layout/Medic/index.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Grid, Container } from 'semantic-ui-react'
+import styled from 'styled-components'
+import SideBar from '../../shared/SideBar/SideBar'
+import SearchBar from '../../shared/SearchBar/SearchBar'
+
+const CustomContainer = styled(Container)`
+  &&& {
+    margin: 0;
+    width: 100%;
+    background-color: #f4f6f8;
+    height: 100vh;
+  }
+`
+
+const CustomGrid = styled(Grid)`
+  &&& {
+    margin: 0;
+  }
+`
+
+const Column = styled(Grid.Column)`
+  &&&& {
+    padding: 0;
+  }
+`
+
+const MainContainer = styled(Container)`
+  &&& {
+    margin: 0;
+    width: 100%;
+    padding: 3rem 3rem 0;
+  }
+`
+
+const Layout = ({ children }) => (
+  <CustomGrid columns={2}>
+    <Column width={1}>
+      <SideBar />
+    </Column>
+    <Column width={15}>
+      <CustomContainer>
+          <SearchBar />
+          <MainContainer>
+            { children }
+          </MainContainer>
+      </CustomContainer>
+    </Column>
+  </CustomGrid>
+)
+
+export default Layout
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,12 +2,16 @@ import App, {Container} from 'next/app'
 import React from 'react'
 import withApolloClient from '../lib/withApolloClient'
 import { ApolloProvider } from 'react-apollo'
+import Header from 'next/head'
 
 class MyApp extends App {
   render () {
     const {Component, pageProps, apolloClient} = this.props
     return (
       <Container>
+        <Header>
+          <title>Health-MX</title>
+        </Header>
         <ApolloProvider client={apolloClient}>
           <Component {...pageProps} />
         </ApolloProvider>
