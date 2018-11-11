aaab51fc5eac37f19a152df87c49204bf3611d2c
Mike <ing.sagnelli@gmail.com>
2018-11-11 12:47:46 -0600

    chore(refactor): refactor login view to fit new theme update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react'
-import Layout from '../components/Layout/index'
+import Layout from '../components/Layout'
+import Login from '../components/Login'
 import { Input, Button, Card, Grid } from 'semantic-ui-react'
 import Link from 'next/link'
 import styled from 'styled-components'
@@ -7,42 +8,43 @@ import styled from 'styled-components'
 class HomePage extends PureComponent {
   render() {
     return (
-      <Layout userRole="medic">
-        <h1 className="ui header">Header 1</h1>
-        <h3 className="ui header">Header 3</h3>
-        <Card>
-          <Card.Content>
-            <Card.Header>
-              Elliot FU
-            </Card.Header>
-            <ExampleGrid>
-              <Grid.Row>
-                <p>Paragraph Example</p>
-              </Grid.Row>
-              <Grid.Row>
-                <Link as="" href=""><a>Link example</a></Link>
-              </Grid.Row>
-              <Grid.Row>
-                <Button secondary>Action button</Button>
-              </Grid.Row>
-              <Grid.Row>
-                <Button primary>Primary button</Button>
-              </Grid.Row>
-              <Grid.Row>
-                <Input placeholder="Basic input"/>
-              </Grid.Row>
-            </ExampleGrid>
-          </Card.Content>
-        </Card>
-      </Layout>
+      <Login />
+      // <Layout userRole="medic">
+      //   <h1 className="ui header">Header 1</h1>
+      //   <h3 className="ui header">Header 3</h3>
+      //   <Card>
+      //     <Card.Content>
+      //       <Card.Header>
+      //         Elliot FU
+      //       </Card.Header>
+      //       <ExampleGrid>
+      //         <Grid.Row>
+      //           <p>Paragraph Example</p>
+      //         </Grid.Row>
+      //         <Grid.Row>
+      //           <Link as="" href=""><a>Link example</a></Link>
+      //         </Grid.Row>
+      //         <Grid.Row>
+      //           <Button secondary>Action button</Button>
+      //         </Grid.Row>
+      //         <Grid.Row>
+      //           <Button primary>Primary button</Button>
+      //         </Grid.Row>
+      //         <Grid.Row>
+      //           <Input placeholder="Basic input"/>
+      //         </Grid.Row>
+      //       </ExampleGrid>
+      //     </Card.Content>
+      //   </Card>
+      // </Layout>
     )
   }
 }
 
 export default HomePage
 
-const ExampleGrid = styled(Grid)`
-  &&&& {
-    width: 250px;
-  }
-`
+// const ExampleGrid = styled(Grid)`
+//   &&&& {
+//     width: 250px;
+//   }
+// `
