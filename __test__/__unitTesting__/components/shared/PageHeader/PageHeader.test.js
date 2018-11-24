8b721a648d3206bd7c841f07ff9cfe4012cbe466
Mike <ing.sagnelli@gmail.com>
2018-11-24 14:13:21 -0600

    chore(tests): apply moment tests

--- __test__/__unitTesting__/components/shared/PageHeader/PageHeader.test.js
+++ __test__/__unitTesting__/components/shared/PageHeader/PageHeader.test.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import { shallow, mount } from 'enzyme'
 import PageHeader from '../../../../../components/shared/PageHeader'
-import wait from 'waait'
 
-Date.now = jest.fn(() => 1487076719000)
+Date.now = jest.fn(() => new Date(Date.UTC(2017, 7, 9, 8)).valueOf())
 
-describe('Header', () => {
+describe('PageHeader', () => {
   it('renders', () => {
     const component = shallow(<PageHeader title="" subtitle="" />)
     expect(component).toMatchSnapshot()
+  })
+
+  it('should call updateTime function', () => {
+    const component = mount(<PageHeader title="" subtitle="" />)
+    const updateTime = component.instance().updateTime
+    expect(component.state('date')).toBe('9 de agosto de 2017')
+    expect(component.state('time')).toBe('03:00 am')
+
+    Date.now = jest.fn(() => new Date(Date.UTC(2017, 7, 9, 9)).valueOf())
+    updateTime()
+    expect(component.state('time')).toBe('04:00 am')
   })
 })
