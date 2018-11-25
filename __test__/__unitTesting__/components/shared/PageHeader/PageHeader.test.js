8a48bd26090b0a696ec3abfc9186888fe4ee7de5
Mike <ing.sagnelli@gmail.com>
2018-11-25 00:08:36 -0600

    chore(tests): apply moment tests

--- __test__/__unitTesting__/components/shared/PageHeader/PageHeader.test.js
+++ __test__/__unitTesting__/components/shared/PageHeader/PageHeader.test.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { shallow, mount } from 'enzyme'
 import PageHeader from '../../../../../components/shared/PageHeader'
+import moment from 'moment-timezone'
 
-Date.now = jest.fn(() => '6/29/2011 4:52:48 PM UTC')
+const date  = moment('2013-07-18T17:00:00', 'YYYY-MM-DD HH:mm', 'Indian/Reunion')
+const date1 = moment('2013-07-18T18:00:00', 'YYYY-MM-DD HH:mm', 'Indian/Reunion')
+Date.now    = jest.fn(() => date)
 
 describe('PageHeader', () => {
   it('renders', () => {
@@ -13,11 +16,11 @@ describe('PageHeader', () => {
   it('should call updateTime function', () => {
     const component = mount(<PageHeader title="" subtitle="" />)
     const updateTime = component.instance().updateTime
-    expect(component.state('date')).toBe('29 de junio de 2011')
-    expect(component.state('time')).toBe('11:52 am')
+    expect(component.state('date')).toBe('18 de julio de 2013')
+    expect(component.state('time')).toBe('05:00 pm')
 
-    Date.now = jest.fn(() => '6/29/2011 4:53:48 PM UTC')
+    Date.now = jest.fn(() => date1)
     updateTime()
-    expect(component.state('time')).toBe('11:53 am')
+    expect(component.state('time')).toBe('06:00 pm')
   })
 })
